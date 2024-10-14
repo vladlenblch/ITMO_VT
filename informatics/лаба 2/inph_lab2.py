f9ea8be40a6c8509f84b108aa1c465866f78ff15
vladlenblch <vlad6larionov@gmail.com>
2024-10-14 10:50:58 +0300

    Update inph_lab2.py

--- informatics/лаба 2/inph_lab2.py
+++ informatics/лаба 2/inph_lab2.py
@@ -1,3 +1,6 @@
+file = open("result.txt", "w")
+
+
 def output():   #основная функция, выводит ответ
     nums = [int(i) for i in message]
     s1 = nums[0] ^ nums[2] ^ nums[4] ^ nums[6]
@@ -8,10 +11,12 @@ def output():   #основная функция, выводит ответ
     bits = {0: 'r1', 1: 'r2', 2: 'i1', 3: 'r3', 4: 'i2', 5: 'i3', 6: 'i4'}
     if s_overall == 0:
         print("Переданное сообщение не содержит ошибок!")
+        file.write("correct")
     else:
         nums[bit] = (nums[bit] + 1) % 2
         print("Ошибка в бите: " + bits[bit])
         print("Верное сообщение: " + str(nums[2]) + str(nums[4]) + str(nums[5]) + str(nums[6]))
+        file.write(str(bits[bit]))
 
 
 def correct_input():    #рекурсивная функция, проверяющая корректность ввода
