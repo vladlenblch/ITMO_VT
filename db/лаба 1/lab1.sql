f2b1655488aad96e9bdbd88244772606714eac97
vlad larionov <vlad6larionov@gmail.com>
2025-02-20 22:00:51 +0300

    Add files via upload

---
+++ db/лаба 1/lab1.sql
@@ -0,0 +1,53 @@
+CREATE TABLE IF NOT EXISTS natural_scene (
+    natural_scene_id SERIAL PRIMARY KEY,
+    planet_id INT NOT NULL,
+    timestamp TIMESTAMP NOT NULL DEFAULT NOW(),
+    FOREIGN KEY (planet_id) REFERENCES planet(planet_id)
+);
+
+CREATE TABLE IF NOT EXISTS planet (
+    planet_id SERIAL PRIMARY KEY,
+    name VARCHAR(64) NOT NULL UNIQUE
+);
+
+CREATE TABLE IF NOT EXISTS phenomena (
+    phenomen_id SERIAL PRIMARY KEY,
+    name VARCHAR(64) NOT NULL,
+    description VARCHAR(64)
+);
+
+CREATE TABLE IF NOT EXISTS consequences (
+    consequence_id SERIAL PRIMARY KEY,
+    consequence VARCHAR(64) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS phenomena_consequences (
+    phenomen_id INT NOT NULL,
+    consequence_id INT NOT NULL,
+    PRIMARY KEY (phenomen_id, consequence_id),
+    FOREIGN KEY (phenomen_id) REFERENCES phenomena(phenomen_id),
+    FOREIGN KEY (consequence_id) REFERENCES consequences(consequence_id)
+);
+
+CREATE TABLE IF NOT EXISTS phenomena_in_scene (
+    natural_scene_id INT NOT NULL,
+    phenomen_id INT NOT NULL,
+    PRIMARY KEY (natural_scene_id, phenomen_id),
+    FOREIGN KEY (natural_scene_id) REFERENCES natural_scene(natural_scene_id),
+    FOREIGN KEY (phenomen_id) REFERENCES phenomena(phenomen_id)
+);
+
+INSERT INTO planet (name) VALUES
+('Юпитер');
+
+INSERT INTO phenomena (name, description) VALUES
+('вихри', NULL), 
+('порывы ветра', 'ураганные'),
+('потоки газа', 'восходящие'),
+('воронка', 'гигантская');
+
+INSERT INTO consequences (consequence) VALUES
+('нарушать строй облаков'),
+('раскрывать пелену'),
+('открывать вид'),
+('низвергаться в глубины');
